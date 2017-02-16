e7221a3f9093abd283d923eb32d3aeee335f605a
Alexander <textbookgladiator@gmail.com>
2017-02-16 15:14:09 -0600

    Updated Upgrade Portion Of HandelVisiter.php
    Single upgrade button has been replaced with button group. This button group is completely auto generated (no constants or variables need to be changed if ticket types change in database). Each button in the group also shows the remaining amount of that ticket type remains.
    
    Note:
    Now takes SQL data retrieved from "php/view/search.php", but still needs to refer form action to "php/view/controller.php".

--- php/view/HandleVisiter.php
+++ php/view/HandleVisiter.php
@@ -5,13 +5,11 @@ name: found
 origin: look-up form
 purpose: boolean value indicating if looked-up user is registered in database
 datatype: boolean
-
 POSIBLE FORM ITEMS SENT:
 { Form: 1st form in code }
 name: action
 value: upgradePerson
 intended datatype: string
-
 { Form: 2nd form in code }
 name: action
 value: registerPerson
@@ -21,46 +19,44 @@ intended datatype: string
 ====== PLACEHOLDER PHP ======
 -->
 <?php
-
 if($_SESSION['found'] == false) {
     $found = false;
 } else {
     $found = true;
 }
-
 ?>
 <!DOCTYPE HTML>
 <html>
   <head> 
     <title>Handle Visiter</title>
-	<!-- Placeholder CSS Classes -->
-	<!--
-	  Note:
-	  Will either be absorbed into a css document
-	  or eliminated completely.
-	-->
+    <!-- Placeholder CSS Classes -->
+    <!--
+      Note:
+      Will either be absorbed into a css document
+      or eliminated completely.
+    -->
     <!-- Bootstrap Related Meta Data -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
-	  .page-vert-space {
-	    height: /*-- VALUE --*/5px;
-	  }
-	  .page-bottom-padding {
+    <style>
+      .page-vert-space {
+        height: /*-- VALUE --*/5px;
+      }
+      .page-bottom-padding {
         height: /*-- VALUE --*/0;
-	  }
-	  .page-top-padding {
-	    height: /*-- VALUE --*/35px;
-	  }
-	</style>
+      }
+      .page-top-padding {
+        height: /*-- VALUE --*/35px;
+      }
+    </style>
     <!--- SCRIPTS --->
     <!--
-	  Placeholder
-	-->
-	<!--- CSS --->
-	<!--
-	  Placeholder
-	-->
+      Placeholder
+    -->
+    <!--- CSS --->
+    <!--
+      Placeholder
+    -->
       <link rel="icon", type="image/x-icon", href="../../images/favicon.ico">
       <link rel="stylesheet" href="../../dist/myStyle.css">
     <!-- TODO: get css myStyle.css to work correctly (panels don't show -->
@@ -69,89 +65,118 @@ if($_SESSION['found'] == false) {
   <body>
     <div class="container">
       <!--- Top Padding --->
-	  <div class="page-top-padding">
-	  </div>
-	  
+      <div class="page-top-padding">
+      </div>
+      
       <!--
         === PAGE MESSAGE ===
       -->
-	  <div class="row">
-		  <div class="col-xs-12">
-			  <div class="panel panel-default">
+      <div class="row">
+          <div class="col-xs-12">
+              <div class="panel panel-default">
                 <div class="panel-heading">
                   Visiter Status  
                 </div>
-				<div class="panel-body">
-				  <p id="msgPanel" class="text-center" style="font-size:20px;">
+                <div class="panel-body">
+                  <p id="msgPanel" class="text-center" style="font-size:20px;">
                     <?php
-				      if($found)
-					  {
-				  	    echo 'Registered';
-					  }
-					  else
-					  {
-					    echo 'Not Registered';
-					  }
+                      if($found)
+                      {
+                        echo 'Registered';
+                      }
+                      else
+                      {
+                        echo 'Not Registered';
+                      }
                     ?>
-				  </p>
-				</div>
-			  </div>
-		  </div>
-	  </div>
-	  
+                  </p>
+                </div>
+              </div>
+          </div>
+      </div>
+      
       <!--- Vertical Spacing --->
-	  <div class="page-vert-space">
-	  </div>
-	  
+      <div class="page-vert-space">
+      </div>
+      
       <!--
         === PAGE BUTTONS ===
       -->
-	  <div class="row">
+      <div class="row">
         <!--- Horizontal Spacer --->
-		<div class="col-xs-3">
+        <div class="col-xs-3">
         </div>
           
-        <!--- Button: Upgrade --->
-		<div class="col-xs-4">
-		    <form action="
-                  <?php
-                    if($found)
-                    {
-                      echo '<!-- URL HERE -->';
-                    }
-                    else
-                    {
-                      /* Exclude form link */
-                    }?>" 
-                  method="post">
-			  <input class="
-                  btn btn-primary
-				  <?php
-				    if( $found )
-					{
-					  /* Keep button enabled */
-					}
-				    else
-					{
-					  /* Disable button */
-					  echo 'disabled';
-					}
-			      ?>"
-				  type="submit"
-			      value="Upgrade">
-			  <input name="action"
-			      type="hidden"
-				  value="upgradePerson">
-			</form>
+        <!--- Button Group: Upgrade --->
+        <div class="col-xs-4">
+	  <?php //*** Label For Button Group *** ?>
+	  <div class="label label-default">
+	    Upgrade Options
+	  </div>
+	  <div class="btn-group-vertical">
+	    <?php
+	    //*** Generate Upgrade Buttons ***
+	    //*******************************?>
+	    <?php
+	    /* *** Outputed Button UI ***
+	     *  ~~~ KEY ~~~
+	     *  #RT ~ Remaining amount of ticket type
+	     *
+	     *  ===========
+	     *
+	     *  _________________________________
+	     * /  Highest Upgrade Option "(#RT)" \  <==== css class: btn btn-primary
+	     * |---------------------------------|
+	     * | ... Lower Options "(#RT)"       |  <=|
+	     * |---------------------------------|
+	     * | ... ... "(#RT)"                 |  <=/
+	     * |---------------------------------|
+	     * | "Current Ticket: [type]"        |  <==== css class: btn btn-default disabled
+	     * \_________________________________/
+	     *
+	     * Note:
+	     * Only posiable upgrades for a visitor is shown or disabled (excluding visitors current ticket type). 
+	     */
+	    ?>
+	    <?php foreach /* thing in */ ($TicketTypeCountQuery AS $TicketType) : ?>
+	      <?php
+		//*** Remember Values Of Ticket Type ***
+		$TicketTypeName = $TicketType[0];
+		$TicketTypePrice = $TicketType[1];
+		$TicketTypeAmount = $TicketType[2];
+	      ?>
+
+	      <?php //*** If Valid Option: Add Button Into HTML *** ?>
+	      <?php if($TicketOfVisitorPrice < $TicketTypePrice): ?>
+		<form action ="<?php if(!found){ echo /*Action URL: */ '<!-- URL HERE -->'; }else{ /*Exclude form link*/ } ?>"
+		      method="post">
+		  <input type="submit"
+			 class="btn btn-primary <?php if($TicketTypeAmount == 0){ echo 'disabled'; } ?>"
+			 value="<?php echo $TicketTypeName.' ('.$TicketTypeAmount.')' ?>">
+		  <input type="hidden"
+			 name="action"
+			 value="registerPerson">
+		  <input type="hidden"
+			 name="ticketTypeID"
+			 value="<?php echo $TicketTypeName ?>">
+		</form>
+	      <?php elseif ($TicketOfVisitorPrice == $TicketTypePrice): ?>
+	      <?php //*** Add Unclickable Button With Visitor's Current Ticket Type *** ?>
+		<div class="btn btn-default disabled">
+		  <?php echo 'Current Ticket: '.$TicketTypeName; ?>
 		</div>
+	      <?php endif; ?>
+	    <?php endforeach; ?>
+	  </div>
+	</div>
         
         <!--- Horizontal Spacer --->
-		<div class="">
-		</div>
+        <div class="">
+        </div>
          
         <!--- Button: Register --->  
-		<div class="col-xs-4">
-		    <form action="
+        <div class="col-xs-4">
+            <form action="
                   <?php
                     if(!$found)
                     {
@@ -163,36 +188,36 @@ if($_SESSION['found'] == false) {
                     }
                   ?>"
                   method="post">
-			     <input class="
-			            btn btn-primary
-				        <?php
-				          if ( !$found ) 
-						  { 
-						    /* Keep button enabled */
-						  }
-				          else
-						  {
-						    /* Disable button */
-						    echo 'disabled';	
-						  }
-				        ?>"
-				        type="submit"
-				        value="Register">
-			    <input name="action"
-			           type="hidden"
-				       value="registerPerson">
-			</form>
-		</div>
+                 <input class="
+                        btn btn-primary
+                        <?php
+                          if ( !$found ) 
+                          { 
+                            /* Keep button enabled */
+                          }
+                          else
+                          {
+                            /* Disable button */
+                            echo 'disabled';
+                          }
+                        ?>"
+                        type="submit"
+                        value="Register">
+                <input name="action"
+                       type="hidden"
+                       value="registerPerson">
+            </form>
+        </div>
           
         <!--- Horizontal Spacer --->
-		<div class="col-xs-1">
-		</div>
-	  </div>
-	
+        <div class="col-xs-1">
+        </div>
+      </div>
+    
       <!-- Vertical Spacing -->
-	  <div class="page-vert-space">
-	  </div>
-	
+      <div class="page-vert-space">
+      </div>
+    
       <!--- Button: Go Back --->
       <!--
       NOTE:
@@ -204,18 +229,18 @@ if($_SESSION['found'] == false) {
       Without the "Go Back" button it is possible
       that the flow of registration could be disrupted.
       -->
-	  <div class="row">
-	    <div class="col-xs-12">
-		  <div class="btn btn-default"
-		       onclick="window.history.back()">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="btn btn-default"
+               onclick="window.history.back()">
             <span class="glyphicon glyphicon-arrow-left"></span>&nbsp;Go Back
           </div>
-	    </div>
-	  </div>
-	
+        </div>
+      </div>
+    
       <!--- Bottom Padding --->
-	  <div class="page-bottom-padding"> 
-	  </div>
+      <div class="page-bottom-padding"> 
+      </div>
         
       <!-- end of bootstrap container -->
     </div>
