93d16e7560c0ba1183d8532fd284b63855ab435a
TheLinuxWizard <mattkwarren@gmail.com>
2017-04-10 15:20:34 -0500

    404
    completed the 404 page. Tell me it isn't cool and I will eat Corey's Mac.

--- php/view/404.php
+++ php/view/404.php
@@ -3,10 +3,94 @@
     <title>Bad url</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="/DIP-Class/dist/myStyle.css" />
-      <link rel="manifest" href="/DIP-Class/manifest.json">
+    <!--<link rel="stylesheet" type="text/css" href="/DIP-Class/dist/myStyle.css">-->
+    <link rel="manifest" href="/manifest.json">
+    
+   	<style>
+		
+		/*fonts*/
+		@import url('https://fonts.googleapis.com/css?family=Raleway');
+		@import url('https://fonts.googleapis.com/css?family=Oswald');
+		@import url('https://fonts.googleapis.com/css?family=Creepster');
+		
+		html, body{
+			margin: 0;
+			padding: 0;
+			height: 100%;
+			font-size: 24px;
+			font-family: 'Creepster', cursive;
+			
+		}
+		
+		#error{
+			color: #AE070A;
+			text-align: center;
+			padding: 0;
+		}
+		
+		.intro{
+			height: 100%;
+			width: 100%;
+			margin: auto;
+			background: url(images/404.jpg) no-repeat 50% 50%;
+			display: table;
+			top: 0;
+			background-size: cover;
+		}
+
+		.intro .inner{
+			display: table-cell;
+			vertical-align: middle;
+			width: 100%;
+			max-width: none;
+		}
+
+		.content{
+			margin: 0 auto;
+			text-align: center;
+		}
+
+		.content h1{
+			font-family: 'Creepster', cursive;
+			color: #ae070a;
+			text-shadow: 0px 0px 300px #000;
+			font-size: 500%;
+		}
+
+		.btnGetMeOutOfHere{
+			border-radius: 9px;
+			font-family: 'Oswald', sans-serif;
+			color: #ae070a;
+			font-size: 135%;
+			padding: 10px 20px;
+			border: solid #ae070a 3px;
+			text-transform: uppercase;
+			text-decoration: none;	
+		}
+		.btnGetMeOutOfHere:hover{
+			 color: lightgray; 
+			border: solid lightgray 3px;
+		}
+		
+		
+		
+	</style>
+          
   </head>
   <body>
-      <h1>Bad url</h1>
+  
+  	<section class="intro">
+		<div class="inner">
+			<div class="content">
+				<h1>You look lost...</h1>
+				<a href="index.php" class="btnGetMeOutOfHere">Get me out of here!</a>
+			</div>				
+		</div>
+	</section>
+  
+	<!--<div>
+		<h1 id="error">You look lost.</h1>
+	</div>-->
+	
   </body>
 </html>
